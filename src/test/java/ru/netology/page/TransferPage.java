de07bbbf155bf1e02a8f0a59e54be2a9d92e6325
alinasadness-cpu <alina_sadness@inbox.ru>
2026-07-29 20:19:36 +0300

    Update TransferPage.java

--- src/test/java/ru/netology/page/TransferPage.java
+++ src/test/java/ru/netology/page/TransferPage.java
@@ -5,22 +5,23 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selenide.$;
 
 public class TransferPage {
+
     private final SelenideElement amountField = $("[data-test-id='amount'] input");
     private final SelenideElement fromField = $("[data-test-id='from'] input");
     private final SelenideElement transferButton = $("[data-test-id='action-transfer']");
     private final SelenideElement cancelButton = $("[data-test-id='action-cancel']");
-
+    
     public TransferPage() {
         $("h1").shouldHave(Condition.text("Пополнение карты"));
     }
-
+    
     public DashboardPage transfer(String amount, String fromCard) {
         amountField.setValue(amount);
         fromField.setValue(fromCard);
         transferButton.click();
         return new DashboardPage();
     }
-
+    
     public DashboardPage cancelTransfer() {
         cancelButton.click();
         return new DashboardPage();

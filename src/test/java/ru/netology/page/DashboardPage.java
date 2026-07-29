0af2d3b5bb7dd028d4403838e78093490899f930
alinasadness-cpu <alina_sadness@inbox.ru>
2026-07-29 22:25:09 +0300

    Update DashboardPage.java

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -8,11 +8,10 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class DashboardPage {
   
-    private ElementsCollection cards = $$(".list_item");
+    private ElementsCollection cards = $$(".list__item");
     
     private final String balanceStart = "баланс: ";
     private final String balanceFinish = " р.";
-    
     
     private SelenideElement refreshButton = $("[data-test-id='action-reload']");
     
@@ -44,7 +43,6 @@ public class DashboardPage {
     }
     
     public TransferPage selectCardForTransfer(int cardIndex) {
-      
         cards.get(cardIndex).$("[data-test-id='action-deposit']").click();
         return new TransferPage();
     }
